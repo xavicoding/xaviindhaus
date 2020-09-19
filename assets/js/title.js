ef39eef104732d96c74ca85fde9ca4672d29cfa6
xavicoding <maki.x455@gmail.com>
2020-09-19 16:26:12 +0200

    first commit

---
+++ assets/js/title.js
@@ -0,0 +1,3 @@
+if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
+function loaded(){setInterval(loop,400);}
+var x=0;var titleText=["R","RE","REN","RENGELE","IN D HAUS","RENGELE IN D HAUS","REN","RE","R"];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
